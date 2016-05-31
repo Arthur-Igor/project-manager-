ca00af9c67ed7ffd8e944dd6afb5be5576ed2ab6
Maykend <maykekend@gmail.com>
2016-05-31 18:52:11 -0300

    Implementações das classes da camada control exceto a classe UsuarioControl

--- src/control/GrupoDeUsuarioControl.java
+++ src/control/GrupoDeUsuarioControl.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package control;
 
 /**
@@ -10,5 +6,32 @@ package control;
  * @author mayke
  */
 public class GrupoDeUsuarioControl {
+    
+     private static GrupoDeUsuarioControl controladorGrupoDeUsuario;
+    
+    //singlegon
+    public static GrupoDeUsuarioControl getInstance(){
+        if(controladorGrupoDeUsuario == null){
+            controladorGrupoDeUsuario = new GrupoDeUsuarioControl();
+            return controladorGrupoDeUsuario;
+        }
+        return controladorGrupoDeUsuario;
+    }
+    
+    public void cadastrarGrupoDeUsuario(){
+    
+    }
+    
+    public void deletarGrupoDeUsuario(){
+    
+    }
+    
+    public void editarGrupoDeUsuario(){
+    
+    }
+    
+    public void listarGrupoDeUsuario(){
+    
+    }
     
 }
--- src/control/UserStoryControl.java
+++ src/control/UserStoryControl.java
@@ -11,4 +11,30 @@ package control;
  */
 public class UserStoryControl {
     
+      private static UserStoryControl controladorUserStory;
+    
+    public static UserStoryControl getInstance(){
+        if(controladorUserStory == null){
+            controladorUserStory = new UserStoryControl();
+            return controladorUserStory;
+        }
+        return controladorUserStory;
+    }
+    
+    public void cadastrarUserStory(){
+    
+    }
+    
+    public void deletarUserStory(){
+    
+    }
+    
+    public void editarUserStory(){
+    
+    }
+    
+    public void listaruserStory(){
+    
+    }
+    
 }
